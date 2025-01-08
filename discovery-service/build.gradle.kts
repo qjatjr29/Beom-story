fdd39ff665535b696cc29292c7b02788c1eec296
beomsic <qjatjr29@khu.ac.kr>
2025-01-08 15:19:34 +0900

    chore: discovery / gateway server에 Docker 설정
    - Docker 빌드를 위한 설정 추가 (Dockerfile 경로 및 bootJar 파일 설정)

--- discovery-service/build.gradle.kts
+++ discovery-service/build.gradle.kts
@@ -9,3 +9,19 @@ dependencyManagement {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
 }
+
+docker {
+    // 이미지 이름 지정
+    val rootProjectName = rootProject.name.lowercase()
+    name = "$rootProjectName-${project.name}:${version}"
+
+    // Dockerfile 경로 지정
+    files("../Dockerfile")
+
+    // 복사할 파일 설정 (bootJar 결과물)
+    val bootJar = tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar").get()
+    files(bootJar.archiveFile.get().asFile)
+
+    // Dockerfile에 전달할 빌드 인자 설정
+    buildArgs(mapOf("JAR_FILE" to bootJar.archiveFileName.get()))
+}
--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -10,3 +10,34 @@ dependencyManagement {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
 }
+
+//docker {
+//    // 이미지 이름 지정
+//    val rootProjectName = rootProject.name.toLowerCase()
+//    name = "$rootProjectName-${project.name}:${version}"
+//
+//    // 어떤 Dockerfile을 사용할지 지정
+//    dockerfile = file("../Dockerfile")
+//
+//    // 복사할 파일 (bootJar 작업의 결과물)
+//    files(tasks.named<Jar>("bootJar").get().outputs.files)
+//
+//    // Dockerfile에 전달할 빌드 인자
+//    buildArgs(mapOf("JAR_FILE" to tasks.named<Jar>("bootJar").get().outputs.files.singleFile.name))
+//}
+
+docker {
+    // 이미지 이름 지정
+    val rootProjectName = rootProject.name.lowercase()
+    name = "$rootProjectName-${project.name}:${version}"
+
+    // Dockerfile 경로 지정
+    files("../Dockerfile")
+
+    // 복사할 파일 설정 (bootJar 결과물)
+    val bootJar = tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar").get()
+    files(bootJar.archiveFile.get().asFile)
+
+    // Dockerfile에 전달할 빌드 인자 설정
+    buildArgs(mapOf("JAR_FILE" to bootJar.archiveFileName.get()))
+}
