665dc813bb468c72d2d62e20113a337bc460b766
beomsic <qjatjr29@khu.ac.kr>
2025-01-08 15:22:17 +0900

    feat: 하위 모듈 UserService 추가
    - settings.gradle에 하위 모듈인 user service 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.palantir.docker") version "0.35.0" apply false
 	kotlin("plugin.jpa") version "1.9.25"
 
-	kotlin("kapt") version "1.6.21"
+	kotlin("kapt") version "1.9.25"
 }
 
 java {
@@ -18,7 +19,7 @@ java {
 
 allprojects {
 	group = "com.beomsic"
-	version = "0.0.1"
+	version = "1.0.0"
 	repositories {
 		mavenCentral()
 	}
@@ -29,6 +30,8 @@ subprojects {
 	apply(plugin = "kotlin-spring")
 	apply(plugin = "io.spring.dependency-management")
 	apply(plugin = "kotlin-kapt")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "com.palantir.docker")
 
 	dependencies {
 		// 로깅 (코틀린 로깅)
@@ -43,6 +46,8 @@ subprojects {
 		implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable")
 
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+		testImplementation("io.projectreactor:reactor-test")
 		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	}
@@ -65,29 +70,14 @@ subprojects {
 	}
 }
 
-//val springCloudVersion = "2024.0.0"
-//
-//dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter")
-//	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
-//
-//
-//dependencyManagement {
-//	imports {
-//		mavenBom ("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
-//	}
-//}
-//
-//kotlin {
-//	compilerOptions {
-//		freeCompilerArgs.addAll("-Xjsr305=strict")
-//	}
-//}
-//
-//tasks.withType<Test> {
-//	useJUnitPlatform()
-//}
+tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+	enabled = false
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	enabled = false
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+	enabled = false
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "beomstory"
 include("discovery-service")
 include("gateway-service")
+include("user-service")
