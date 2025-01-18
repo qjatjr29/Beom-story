f829d700c9c5e6c4deff4b6d0c1033d4bde291e0
beomsic <qjatjr29@khu.ac.kr>
2025-01-18 16:24:23 +0900

    remove: 사용하지 않는 import 제거

--- user-service/src/test/kotlin/com/beomsic/userservice/adapter/in/web/UserFindControllerTest.kt
+++ user-service/src/test/kotlin/com/beomsic/userservice/adapter/in/web/UserFindControllerTest.kt
@@ -11,8 +11,6 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
