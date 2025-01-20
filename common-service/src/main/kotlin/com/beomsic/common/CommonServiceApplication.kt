71df8e7afd0a42cb1942c28dec531c4c1b1714b8
beomsic <qjatjr29@khu.ac.kr>
2025-01-20 12:20:02 +0900

    feat: 공통모듈 서비스

---
+++ common-service/src/main/kotlin/com/beomsic/common/CommonServiceApplication.kt
@@ -0,0 +1,10 @@
+package com.beomsic.common
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommonServiceApplication
+fun main(args: Array<String>) {
+    runApplication<CommonServiceApplication>(*args)
+}
