edeaf58b0ae833040bee4c14fe4f76fb31205aad
beomsic <hhqjatjr@gmail.com>
2025-02-12 15:27:39 +0900

    feat: 장소 커스텀 예외 추가

--- place-service/src/main/kotlin/com/beomsic/placeservice/domain/exception/PlaceException.kt
+++ place-service/src/main/kotlin/com/beomsic/placeservice/domain/exception/PlaceException.kt
@@ -6,6 +6,16 @@ sealed class PlaceException(
     cause: Throwable? = null
 ) : RuntimeException(message, cause)
 
+data class PlaceNotFoundException(
+    override val message: String = "장소가 존재하지 않습니다.",
+) : PlaceException(404, message)
+
+data class ForbiddenException(
+    override val message: String = "요청에 대한 권한이 없습니다.",
+) : PlaceException(403, message)
+
+data class InvalidException(
+    override val message: String = "잘못된 값이 들어왔습니다.",
+) : PlaceException(400, message)
 
 class ServerException(message: String, cause: Throwable? = null): PlaceException(500, message, cause)
-class PlaceNotFoundException(message: String, cause: Throwable? = null) : PlaceException(404, message, cause)
