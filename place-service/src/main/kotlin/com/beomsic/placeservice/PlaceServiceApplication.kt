0813c765864f7c8ff83e54184231e601649b0a9a
beomsic <hhqjatjr@gmail.com>
2025-02-12 15:23:45 +0900

    feat: EnableR2dbcAuditing 설정 추가

--- place-service/src/main/kotlin/com/beomsic/placeservice/PlaceServiceApplication.kt
+++ place-service/src/main/kotlin/com/beomsic/placeservice/PlaceServiceApplication.kt
@@ -2,10 +2,13 @@ package com.beomsic.placeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableR2dbcAuditing
 class PlaceServiceApplication
 fun main(args: Array<String>) {
     runApplication<PlaceServiceApplication>(*args)
