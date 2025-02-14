1b9b5043c4c8e1466fc43b5f868ad6a831aa86ba
beomsic <hhqjatjr@gmail.com>
2025-02-14 16:36:56 +0900

    feat: 잘못된 값이 들어왔을 경우의 exception 추가

--- user-service/src/main/kotlin/com/beomsic/userservice/domain/exception/ServerException.kt
+++ user-service/src/main/kotlin/com/beomsic/userservice/domain/exception/ServerException.kt
@@ -17,6 +17,10 @@ data class PasswordNotMatchedException(
     override val message: String = "패스워드가 잘못되었습니다."
 ) : ServerException(409, message)
 
+data class InvalidException(
+    override val message: String = "잘못된 형식의 값이 들어왔습니다."
+) : ServerException(400, message)
+
 data class InvalidJwtTokenException(
     override val message: String = "잘못된 토큰입니다."
 ) : ServerException(400, message)
