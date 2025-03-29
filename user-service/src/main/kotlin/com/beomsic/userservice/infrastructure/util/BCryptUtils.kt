0ed4d7d64d5b41fb65c40b03a480c766ec7a3c5f
beomsic <hhqjatjr@gmail.com>
2025-03-29 13:11:42 +0900

    remove: 사용하지 않는 주석 삭제

--- user-service/src/main/kotlin/com/beomsic/userservice/infrastructure/util/BCryptUtils.kt
+++ user-service/src/main/kotlin/com/beomsic/userservice/infrastructure/util/BCryptUtils.kt
@@ -4,16 +4,9 @@ import at.favre.lib.crypto.bcrypt.BCrypt
 
 object BCryptUtils {
 
-    /**
-     * 해시 함수
-     */
-    fun hash(password: String) =
+    fun hash(password: String): String =
         BCrypt.withDefaults().hashToString(12, password.toCharArray())
 
-    /**
-     * 요청으로 들어온 평문문자열과 데이터베이스에 있는 해시 함수를 비교
-     * 검증
-     */
     fun verify(password: String, hashedPassword: String) =
         BCrypt.verifyer().verify(password.toCharArray(), hashedPassword).verified
 }
