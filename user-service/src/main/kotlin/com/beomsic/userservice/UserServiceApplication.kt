c0f0d3324aa2307bf0ac4af1f75c0f7ec0683d57
beomsic <hhqjatjr@gmail.com>
2025-03-29 13:12:09 +0900

    feat: @EnableR2dbcAuditing

--- user-service/src/main/kotlin/com/beomsic/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/com/beomsic/userservice/UserServiceApplication.kt
@@ -3,9 +3,11 @@ package com.beomsic.userservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableR2dbcAuditing
 class UserServiceApplication
 fun main(args: Array<String>) {
 	runApplication<UserServiceApplication>(*args)
