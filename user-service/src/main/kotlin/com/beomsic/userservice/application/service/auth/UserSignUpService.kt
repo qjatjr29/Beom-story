cbe93f6c33ccd65cb1624324908ccad617e03af0
beomsic <hhqjatjr@gmail.com>
2025-04-02 01:24:06 +0900

    feat: @Transactional 추가

--- user-service/src/main/kotlin/com/beomsic/userservice/application/service/auth/UserSignUpService.kt
+++ user-service/src/main/kotlin/com/beomsic/userservice/application/service/auth/UserSignUpService.kt
@@ -5,6 +5,7 @@ import com.beomsic.userservice.application.port.`in`.usecase.UserSignUpUseCase
 import com.beomsic.userservice.application.port.out.UserSignUpPort
 import com.beomsic.userservice.application.service.ValidationService
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserSignUpService(
@@ -12,6 +13,7 @@ class UserSignUpService(
     private val validationService: ValidationService,
 ) : UserSignUpUseCase {
 
+    @Transactional
     override suspend fun execute(command: UserSignUpCommand) {
         with(command) {
             validationService.validateEmail(email)
