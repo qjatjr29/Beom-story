d32ae93dcd7fc8acbbca7d9723bc637a37a99584
beomsic <hhqjatjr@gmail.com>
2025-04-07 00:44:14 +0900

    feat: place category 추가

--- place-service/src/main/kotlin/com/beomsic/placeservice/domain/Category.kt
+++ place-service/src/main/kotlin/com/beomsic/placeservice/domain/Category.kt
@@ -1,13 +1,18 @@
 package com.beomsic.placeservice.domain
 
-enum class Category {
-    음식점,
-    카페,
-    관광지,
-    기타;
+enum class Category (val value: String) {
+    RESTAURANT("음식점"),
+    CAFE("카페"),
+    ACCOMMODATION("숙박"),
+    ATTRACTION("관광명소"),
+    SHOPPING("쇼핑"),
+    CULTURE("문화/공연"),
+    TRANSPORT("교통"),
+    OTHER("기타");
 
     companion object {
-        fun fromString(value: String): Category =
-            enumValues<Category>().find { it.name == value } ?: 기타
+        fun fromValue(value: String): Category {
+            return entries.find { it.value == value } ?: OTHER
+        }
     }
 }
