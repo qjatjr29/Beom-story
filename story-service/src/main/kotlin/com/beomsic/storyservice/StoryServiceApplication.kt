4d80df1a76276af94826446ce291fb1dcb446f7b
beomsic <hhqjatjr@gmail.com>
2025-04-08 11:02:12 +0900

    chore: 사용하지 않는 설정 삭제 및 필요한 어노테이션 적용

--- story-service/src/main/kotlin/com/beomsic/storyservice/StoryServiceApplication.kt
+++ story-service/src/main/kotlin/com/beomsic/storyservice/StoryServiceApplication.kt
@@ -3,9 +3,13 @@ package com.beomsic.storyservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
+@EnableR2dbcAuditing
 class StoryServiceApplication
 fun main(args: Array<String>) {
     runApplication<StoryServiceApplication>(*args)
--- story-service/src/main/kotlin/com/beomsic/storyservice/infrastructure/config/WebConfig.kt
+++ story-service/src/main/kotlin/com/beomsic/storyservice/infrastructure/config/WebConfig.kt
@@ -1,7 +1,6 @@
 package com.beomsic.storyservice.infrastructure.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
 
@@ -13,11 +12,5 @@ class WebConfig(
     override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
         super.configureArgumentResolvers(configurer)
         configurer.addCustomResolver(authTokenResolver)
-    }
-
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("*")
     }
 }
