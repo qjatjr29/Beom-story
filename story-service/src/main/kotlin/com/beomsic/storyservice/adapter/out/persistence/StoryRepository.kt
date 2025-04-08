5c3d3f7e45893703e17aef278ad93d52f4722e86
beomsic <hhqjatjr@gmail.com>
2025-04-08 11:06:19 +0900

    feat: 페이지네이션을 위한 custom repository implements 추가

--- story-service/src/main/kotlin/com/beomsic/storyservice/adapter/out/persistence/StoryRepository.kt
+++ story-service/src/main/kotlin/com/beomsic/storyservice/adapter/out/persistence/StoryRepository.kt
@@ -1,6 +1,11 @@
 package com.beomsic.storyservice.adapter.out.persistence
 
+import com.beomsic.storyservice.domain.exception.StoryNotFoundException
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface StoryRepository : CoroutineCrudRepository<StoryEntity, Long> {
+interface StoryRepository : CoroutineCrudRepository<StoryEntity, Long>, StoryPaginationRepository {
+}
+
+suspend fun StoryRepository.findByIdOrNull(id: Long): StoryEntity {
+    return findById(id) ?: throw StoryNotFoundException()
 }
