7f625de4de8e5987398e592a40e78daca77bfbb5
beomsic <hhqjatjr@gmail.com>
2025-04-08 11:05:47 +0900

    feat: Story -> Story Response 변환 / 요약 response 추가

--- story-service/src/main/kotlin/com/beomsic/storyservice/adapter/in/web/StoryResponse.kt
+++ story-service/src/main/kotlin/com/beomsic/storyservice/adapter/in/web/StoryResponse.kt
@@ -1,5 +1,6 @@
 package com.beomsic.storyservice.adapter.`in`.web
 
+import com.beomsic.storyservice.domain.model.Story
 import java.time.LocalDateTime
 
 data class StoryDetailResponse(
@@ -12,4 +13,43 @@ data class StoryDetailResponse(
     val endDate: LocalDateTime,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime
-)
+) {
+    companion object {
+        operator fun invoke(story: Story) = with(story) {
+            StoryDetailResponse(
+                id = id,
+                authorId = authorId,
+                title = title,
+                description = description,
+                category = category.name,
+                startDate = startDate,
+                endDate = endDate,
+                createdAt = createdAt,
+                updatedAt = updatedAt
+            )
+        }
+    }
+}
+
+data class StorySummaryResponse(
+    val id: Long,
+    val authorId: Long,
+    val title: String,
+    val category: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val createdAt: LocalDateTime,
+) {
+    companion object {
+        operator fun invoke(story: Story) = with(story) {
+            StorySummaryResponse(
+                id = id,
+                authorId = authorId,
+                title = title,
+                category = category.name,
+                startDate = startDate,
+                endDate = endDate,
+                createdAt = createdAt,
+            )
+        }}
+}
