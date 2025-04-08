c7451cbf7df9381eb03ed1a172aebc7e34ce5c17
beomsic <hhqjatjr@gmail.com>
2025-04-08 11:05:16 +0900

    remove

--- story-service/src/main/kotlin/com/beomsic/storyservice/infrastructure/persistence/StoryRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.beomsic.storyservice.infrastructure.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface StoryRepository : JpaRepository<StoryEntity, Long> {
-}
