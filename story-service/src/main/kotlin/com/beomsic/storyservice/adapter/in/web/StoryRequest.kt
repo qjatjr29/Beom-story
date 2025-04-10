225ff4efa25524950d2cf91b219555d3c6e0363b
beomsic <hhqjatjr@gmail.com>
2025-04-10 16:14:33 +0900

    feat: story 업데이트를 위한 reqest 추가

--- story-service/src/main/kotlin/com/beomsic/storyservice/adapter/in/web/StoryRequest.kt
+++ story-service/src/main/kotlin/com/beomsic/storyservice/adapter/in/web/StoryRequest.kt
@@ -6,6 +6,14 @@ data class StoryCreateRequest(
     val title: String,
     val description: String?,
     val category: String,
-    val startDate: LocalDateTime?,
-    val endDate: LocalDateTime?,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+)
+
+data class StoryUpdateRequest(
+    val title: String,
+    val description: String?,
+    val category: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
 )
