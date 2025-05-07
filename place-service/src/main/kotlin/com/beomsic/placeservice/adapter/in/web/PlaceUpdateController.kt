608b44ce70700d8931dfa69f1633daad803373ca
beomsic <hhqjatjr@gmail.com>
2025-05-07 16:19:25 +0900

    refactor: 이미지 수정후 반환 코드 설정

--- place-service/src/main/kotlin/com/beomsic/placeservice/adapter/in/web/PlaceUpdateController.kt
+++ place-service/src/main/kotlin/com/beomsic/placeservice/adapter/in/web/PlaceUpdateController.kt
@@ -5,15 +5,10 @@ import com.beomsic.common.model.AuthUser
 import com.beomsic.placeservice.application.port.`in`.command.PlaceUpdateCommand
 import com.beomsic.placeservice.application.port.`in`.usecase.PlaceUpdateUseCase
 import com.beomsic.placeservice.domain.Category
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/place-service")
@@ -32,6 +27,7 @@ class PlaceUpdateController(
     }
 
     @PatchMapping("/{placeId}/image")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     suspend fun updateImage(
         @AuthToken authUser: AuthUser,
         @PathVariable("placeId") placeId: Long,
