907c61c66228930e6273fc1ae58fad35bf300db1
beomsic <hhqjatjr@gmail.com>
2025-05-07 16:18:33 +0900

    test: 장소 삭제 테스트 추가

---
+++ place-service/src/test/kotlin/com/beomsic/placeservice/adapter/out/persistence/PlaceDeleteAdapterTest.kt
@@ -0,0 +1,70 @@
+package com.beomsic.placeservice.adapter.out.persistence
+
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.*
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+class PlaceDeleteAdapterTest: BehaviorSpec({
+
+    val placeRepository = mockk<PlaceRepository>()
+    val placeDeleteAdapter = PlaceDeleteAdapter(placeRepository)
+
+    afterEach { clearAllMocks() }
+
+    given("placeId를 통한 장소 삭제 메소드가 호출될 때") {
+        val placeId = 1L
+
+        `when`("정상적으로 삭제가 완료되면") {
+            coEvery { placeRepository.deleteById(placeId) } just Runs
+
+            then("예외가 발생하지 않고 삭제가 실행된다.") {
+                shouldNotThrow<Exception> {
+                    placeDeleteAdapter.deletePlace(placeId)
+                }
+                coVerify(exactly = 1) { placeRepository.deleteById(placeId) }
+            }
+        }
+
+        `when`("삭제 중 예외가 발생하면") {
+            coEvery { placeRepository.deleteById(placeId) } throws  RuntimeException()
+
+            then("예외가 발생 한다") {
+                shouldThrow<RuntimeException> {
+                    placeDeleteAdapter.deletePlace(placeId)
+                }
+                coVerify(exactly = 1) { placeRepository.deleteById(placeId) }
+            }
+        }
+    }
+
+    given("storyId를 통해 story에 있는 모든 장소를 삭제하는 메소드가 호출되면") {
+        val storyId = 1L
+
+        `when`("정상적으로 삭제가 완료되면") {
+            coEvery { placeRepository.deleteAllByStoryId(storyId) } just Runs
+
+            then("예외가 발생하지 않고 삭제되어야 한다.") {
+                shouldNotThrow<Exception> {
+                    placeDeleteAdapter.deleteAllByStoryId(storyId)
+                }
+
+                coVerify(exactly = 1) { placeRepository.deleteAllByStoryId(storyId) }
+            }
+        }
+
+        `when`("삭제 중 예외가 발생하면") {
+            coEvery { placeRepository.deleteAllByStoryId(storyId) } throws RuntimeException()
+
+            then("예외가 전파되어야 한다") {
+               shouldThrow<RuntimeException> {
+                    placeDeleteAdapter.deleteAllByStoryId(storyId)
+                }
+                coVerify(exactly = 1) { placeRepository.deleteAllByStoryId(storyId) }
+            }
+        }
+    }
+
+})
---
+++ place-service/src/test/kotlin/com/beomsic/placeservice/application/service/PlaceDeleteServiceTest.kt
@@ -0,0 +1,176 @@
+package com.beomsic.placeservice.application.service
+
+
+import com.beomsic.placeservice.application.port.out.ImageRollbackOutboxPort
+import com.beomsic.placeservice.application.port.out.PlaceDeletePort
+import com.beomsic.placeservice.application.port.out.PlaceFindPort
+import com.beomsic.placeservice.domain.Category
+import com.beomsic.placeservice.domain.Place
+import com.beomsic.placeservice.domain.exception.PlaceNotFoundException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.*
+import org.springframework.test.context.ActiveProfiles
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@ActiveProfiles("test")
+class PlaceDeleteServiceTest: BehaviorSpec({
+    val placeDeletePort = mockk<PlaceDeletePort>()
+    val placeFindPort = mockk<PlaceFindPort>()
+    val imageRollbackOutboxPort = mockk<ImageRollbackOutboxPort>()
+
+    val placeDeleteService = PlaceDeleteService(
+        placeDeletePort = placeDeletePort,
+        placeFindPort = placeFindPort,
+        imageRollbackOutboxPort = imageRollbackOutboxPort
+    )
+
+    val placeId = 1L
+    val storyId = 2L
+    val authorId = 3L
+    val currentImageUrl = "image/image.jpg"
+
+    fun makeMockPlace(placeId: Long = 1L, storyId: Long = 2L, authorId: Long = 3L, imageUrl: String? = null): Place {
+        return mockk<Place>(relaxed = true) {
+            every { this@mockk.id } returns placeId
+            every { this@mockk.storyId } returns storyId
+            every { this@mockk.authorId } returns authorId
+            every { this@mockk.name } returns "place name"
+            every { this@mockk.description } returns "place description"
+            every { this@mockk.imageUrl } returns imageUrl
+            every { this@mockk.category } returns Category.ATTRACTION
+            every { this@mockk.latitude } returns 2.0
+            every { this@mockk.longitude } returns 2.0
+            every { this@mockk.address } returns "place address"
+            every { this@mockk.visitedDate } returns LocalDate.of(2025, 5, 1)
+            every { this@mockk.createdAt } returns LocalDateTime.of(2025, 5, 2, 11, 11, 11)
+            every { this@mockk.updatedAt } returns LocalDateTime.of(2025, 5, 2, 11, 11, 11)
+        }
+    }
+
+    afterEach {
+        clearAllMocks()
+    }
+
+    given("장소 ID를 통한 장소 삭제 시") {
+
+        val mockPlace = makeMockPlace(imageUrl = currentImageUrl)
+
+        `when`("이미지가 존재하는 장소인 경우") {
+
+            coEvery { placeFindPort.findByPlaceId(placeId) } returns mockPlace
+            coEvery { placeDeletePort.deletePlace(placeId) } just Runs
+            coEvery { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) } just Runs
+
+            then("deletePlace가 호출되고 이미지 rollback 메시지가 저장된다") {
+
+                placeDeleteService.execute(placeId)
+
+                coVerify { placeDeletePort.deletePlace(placeId) }
+                coVerify {
+                    imageRollbackOutboxPort.saveImageRollbackMessage(
+                        placeId = placeId,
+                        imageUrl = currentImageUrl
+                    )
+                }
+            }
+        }
+
+        `when`("이미지가 없는 장소 삭제") {
+            val place : Place = makeMockPlace()
+            coEvery { placeFindPort.findByPlaceId(placeId) } returns place
+            coEvery { placeDeletePort.deletePlace(placeId) } just Runs
+
+            then("deletePlace는 호출되지만 rollback 메시지는 저장되지 않는다") {
+                placeDeleteService.execute(placeId)
+                coVerify { placeDeletePort.deletePlace(placeId) }
+                coVerify(exactly = 0) { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) }
+            }
+        }
+
+        `when`("없는 장소를 삭제하려는 경우") {
+
+            coEvery { placeFindPort.findByPlaceId(placeId) } throws PlaceNotFoundException()
+
+            then("404 예외가 반환된다.") {
+                shouldThrow<PlaceNotFoundException> {
+                    placeDeleteService.execute(placeId)
+                }.message shouldBe PlaceNotFoundException().message
+
+                coVerify { placeFindPort.findByPlaceId(placeId) }
+                coVerify(exactly = 0) { placeDeletePort.deletePlace(placeId) }
+                coVerify(exactly = 0) { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) }
+            }
+        }
+
+        `when`("장소 삭제과정에서 예외(에러)가 발생하는 경우") {
+
+            coEvery { placeFindPort.findByPlaceId(placeId) } returns mockPlace
+            coEvery { placeDeletePort.deletePlace(placeId) } throws RuntimeException()
+
+            then("해당 예외가 반환된다.") {
+                shouldThrow<RuntimeException> {
+                    placeDeleteService.execute(placeId)
+                }
+                coVerify(exactly = 1) { placeFindPort.findByPlaceId(placeId) }
+                coVerify(exactly = 1) { placeDeletePort.deletePlace(placeId) }
+                coVerify(exactly = 0) { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) }
+            }
+        }
+    }
+
+    given("스토리 ID를 통한 스토리에 있는 모든 장소를 삭제하는 경우") {
+
+        val imageUrl1 = "http://image.com/1.jpg"
+        val imageUrl3 = "http://image.com/3.jpg"
+        val mockPlaces = listOf(
+            makeMockPlace(placeId = 1L, imageUrl = imageUrl1),
+            makeMockPlace(placeId = 2L),
+            makeMockPlace(placeId = 3L, imageUrl = imageUrl3)
+        )
+
+        `when`("여러 장소 중 일부에만 이미지가 존재하면") {
+
+            coEvery { placeFindPort.findAllByStoryId(storyId) } returns mockPlaces
+            coEvery { placeDeletePort.deleteAllByStoryId(storyId) } just Runs
+            coEvery { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) } just Runs
+
+            then("삭제 메소드 호출 이후 이미지가 있는 장소에 대해서만 rollback 메시지 저장") {
+
+                placeDeleteService.deleteAllByStoryId(storyId)
+
+                coVerify { placeDeletePort.deleteAllByStoryId(storyId) }
+
+                coVerify {
+                    imageRollbackOutboxPort.saveImageRollbackMessage(1L, imageUrl1)
+                    imageRollbackOutboxPort.saveImageRollbackMessage(3L, imageUrl3)
+                }
+
+                coVerify(exactly = 0) {
+                    imageRollbackOutboxPort.saveImageRollbackMessage(2L, any())
+                }
+            }
+        }
+
+        `when`("모든 장소가 imageUrl이 없는 경우") {
+
+            val mockNonImagePlaces = listOf(
+                makeMockPlace(placeId = 1L),
+                makeMockPlace(placeId = 2L),
+                makeMockPlace(placeId = 3L)
+            )
+
+            coEvery { placeFindPort.findAllByStoryId(storyId) } returns mockNonImagePlaces
+            coEvery { placeDeletePort.deleteAllByStoryId(storyId) } just Runs
+
+            then("rollback 메시지는 저장되지 않는다") {
+                placeDeleteService.deleteAllByStoryId(storyId)
+                coVerify { placeDeletePort.deleteAllByStoryId(storyId) }
+                coVerify(exactly = 0) { imageRollbackOutboxPort.saveImageRollbackMessage(any(), any()) }
+            }
+        }
+    }
+
+})
