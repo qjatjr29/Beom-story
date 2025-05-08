d38935a1d0766abe41d813be05ba8b615b35f117
beomsic <hhqjatjr@gmail.com>
2025-05-08 13:57:57 +0900

    feat: 커스텀 예외 추가

--- place-service/src/main/kotlin/com/beomsic/placeservice/domain/exception/PlaceException.kt
+++ place-service/src/main/kotlin/com/beomsic/placeservice/domain/exception/PlaceException.kt
@@ -14,6 +14,10 @@ data class ForbiddenException(
     override val message: String = "요청에 대한 권한이 없습니다.",
 ) : PlaceException(403, message)
 
+data class UnauthorizedPlaceAccessException(
+    override val message: String = "요청에 대한 권한이 없습니다.",
+) : PlaceException(403, message)
+
 data class InvalidException(
     override val message: String = "잘못된 값이 들어왔습니다.",
 ) : PlaceException(400, message)
